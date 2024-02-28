253fd9b025d37f6f0520195cf3bf2e3a7c1d0eba
Julian Osorio <jaosoriom27@gmail.com>
2024-02-28 10:51:26 -0500

    Se realiza un boton personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
-
   static const String name = 'buttons_screen';
 
   const ButtonsScreen({super.key});
@@ -16,11 +15,10 @@ class ButtonsScreen extends StatelessWidget {
       body: const _ButtonsView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_back_ios_new_rounded),
-        onPressed: () { 
+        onPressed: () {
           // Volvemos a la pantalla anterior
           context.pop();
-         },
-        
+        },
       ),
     );
   }
@@ -31,9 +29,7 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final colors =Theme.of(context).colorScheme;
-
+    final colors = Theme.of(context).colorScheme;
 
     return SizedBox(
       width: double.infinity,
@@ -44,46 +40,70 @@ class _ButtonsView extends StatelessWidget {
           alignment: WrapAlignment.center,
           children: [
             ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
-            const ElevatedButton(onPressed: null, child: Text('Elevated Disable')),
-            ElevatedButton.icon(onPressed: () {
-              
-            }, icon: const Icon(Icons.accessibility_sharp),
-             label: const Text('Elevated Icon')),
-
-             FilledButton(onPressed: () {
-               
-             }, child: const Text('Filled')),
-
-             FilledButton.icon(onPressed: () {} ,
-              icon: const Icon(Icons.scale_outlined),
-              label: const Text('Filled Icon')),
-
-              OutlinedButton(onPressed: () {},
-               child: const Text('Outline')),
-
-              OutlinedButton.icon(onPressed: () {},
-               icon: const Icon(Icons.add_moderator_sharp),
-              label: const Text('Outline Icon')), 
-
-              TextButton(onPressed: () {}, child: const Text('Text')),
-              TextButton.icon(onPressed: () {}, 
-              icon: const Icon(Icons.workspaces), 
-              label: const Text('Text Icon')),
-
-              // TODO: Custom button
-
-              IconButton(onPressed: () {}, icon: const Icon(Icons.app_registration_rounded)),
-
-              IconButton(onPressed: () {},
-               icon: const Icon(Icons.webhook),
-               style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(colors.primary),
-                iconColor: const MaterialStatePropertyAll( Colors.white )
-               ),
-                )
-
-
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated Disable')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.accessibility_sharp),
+                label: const Text('Elevated Icon')),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.scale_outlined),
+                label: const Text('Filled Icon')),
+            OutlinedButton(onPressed: () {}, child: const Text('Outline')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add_moderator_sharp),
+                label: const Text('Outline Icon')),
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.workspaces),
+                label: const Text('Text Icon')),
+            const CustomButton(),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.app_registration_rounded)),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.webhook),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(colors.primary),
+                  iconColor: const MaterialStatePropertyAll(Colors.white)),
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+// Boton custom
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    // Recorta o realiza bordes redondeados
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        // Reacciona al dar click sobre el boton 
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
